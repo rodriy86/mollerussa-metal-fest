703cf354f7cf285798d3bbd37fec9ece119ea68b
rodriy86 <52785747+rodriy86@users.noreply.github.com>
2026-02-15 02:02:02 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -585,8 +585,8 @@ const server = http.createServer((req, res) => {
 // Función para enviar email de comida solidaria
 const enviarEmailComidaSolidaria = async (formData) => {
   try {
-    const nodemailer = await import('nodemailer');
-    //const nodemailer = require('nodemailer');
+    //const nodemailer = await import('nodemailer');
+    const nodemailer = require('nodemailer');
 
     const transporter = nodemailer.createTransport({
       service: 'gmail',
